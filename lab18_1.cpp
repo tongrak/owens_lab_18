a57fd072f195b045443d63d480f134e30bc21a84
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2021-02-23 16:10:26 +0700

    Lab18_1&2 done!!

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,23 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect A, Rect B){
+	double x, y;
+	//Overlaping case
+	if(B.x>A.x&&B.y<A.y&&(A.x + A.w)>(B.x + B.w)&&(A.y + A.h)>(B.y + B.h))return (B.w-B.x)*(B.h-B.x);
+	else if(B.x<A.x&&B.y>A.y&&(A.x + A.w)<(B.x + B.w)&&(A.y + A.h)<(B.y + B.h))return (A.w-A.x)*(A.h-A.x);
+	//Normal interserting case
+	x=min((A.x + A.w),(B.x + B.w))-max(A.x,B.x);
+	y=min(A.y,B.y)-max((A.y-A.h),(B.y - B.h));
+	//checking for non-intersecting case
+	if(x*y<0)return 0;
+	else return x*y;
+}
 
+int main(){
+	Rect R1 = {-1,2,6.9,9.6};
+	Rect R2 = {0,0,10.2,2.5};
+	cout << overlap(R1,R2);
 
-
+	return 0;
 }
--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,16 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *A, Rect *B){
+	double x, y;
+	//Overlaping case
+	if(B->x>A->x&&B->y<A->y&&(A->x + A->w)>(B->x + B->w)&&(A->y + A->h)>(B->y + B->h))return (B->w-B->x)*(B->h-B->x);
+	else if(B->x<A->x&&B->y>A->y&&(A->x + A->w)<(B->x + B->w)&&(A->y + A->h)<(B->y + B->h))return (A->w-A->x)*(A->h-A->x);
+	//Normal interserting case
+	x=min((A->x + A->w),(B->x + B->w))-max(A->x,B->x);
+	y=min(A->y,B->y)-max((A->y-A->h),(B->y - B->h));
+	//checking for non-intersecting case
+	if(x*y<0)return 0;
+	else return x*y;
 }
+
